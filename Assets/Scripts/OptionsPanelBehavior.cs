b40cf827905e62f82645905d80d7943ab843ee88
Michael Vittiglio <michael.vittiglio@gmail.com>
2020-11-23 17:20:49 -0500

    Options menu complete. Time to add the LD Client. :-)

---
+++ Assets/Scripts/OptionsPanelBehavior.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPanelBehavior : MonoBehaviour
+{
+
+	public const string LdMobileKeyKey = "ld-mobile-key";
+
+	public InputField MobileKeyInput;
+	public void Show()
+	{
+		if (MobileKeyInput != null)
+		{
+			if (PlayerPrefs.HasKey(LdMobileKeyKey))
+			{
+				MobileKeyInput.SetTextWithoutNotify(PlayerPrefs.GetString(LdMobileKeyKey));
+			}
+			else
+			{
+				MobileKeyInput.SetTextWithoutNotify("mob-xxx-xxxxxxx");
+			}
+		}
+
+		gameObject.SetActive(true);
+	}
+
+	public void Apply()
+	{
+		if (MobileKeyInput != null)
+		{
+			PlayerPrefs.SetString(LdMobileKeyKey, MobileKeyInput.text);
+		}
+
+		gameObject.SetActive(false);
+	}
+
+	public void Discard()
+	{
+		if (MobileKeyInput != null)
+		{
+			if (PlayerPrefs.HasKey(LdMobileKeyKey))
+			{
+				MobileKeyInput.SetTextWithoutNotify(PlayerPrefs.GetString(LdMobileKeyKey));
+			}
+			else
+			{
+				MobileKeyInput.SetTextWithoutNotify("mob-xxx-xxxxxxx");
+			}
+		}
+
+		gameObject.SetActive(false);
+	}
+}
